013a56c59001250676bada54096f1d7a5d70d94e
harris <khanharris7@yahoo.com>
2021-05-05 22:01:53 -0500

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const newHeader = document.createElement('H1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", 'victory');
+
+// const firstName = "Emmanuel";
+
+newHeader.innerHTML = `Emmanuel is the champion`;
